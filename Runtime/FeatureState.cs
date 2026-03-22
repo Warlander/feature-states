e4c4ba07c20d92a334b5453dc7a123316354f257
Warlander <mackordian@gmail.com>
2026-03-22 06:42:18 +0100

    Moved feature states into their dedicated local package

---
+++ Runtime/FeatureState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Warlogic.Features
+{
+    [Serializable]
+    public struct FeatureState
+    {
+        [SerializeField] private string _featureName;
+        [SerializeField] private bool _enabledInProduction;
+        [SerializeField] private bool _enabledInDebug;
+        [SerializeField] private bool _enabledInEditor;
+
+        public string FeatureName => _featureName;
+        public bool EnabledInProduction => _enabledInProduction;
+        public bool EnabledInDebug => _enabledInDebug;
+        public bool EnabledInEditor => _enabledInEditor;
+
+        public FeatureState(string featureName)
+        {
+            _featureName = featureName;
+            _enabledInProduction = false;
+            _enabledInDebug = false;
+            _enabledInEditor = false;
+        }
+    }
+}
